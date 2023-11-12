1ca399e2a00c0f858b921a5b5a9bd5dc6fdd8b89
Viktor Stashchuk <vstashchuk@reenbit.com>
2023-11-12 19:39:06 +0200

    Implement retrieving new scrambled word

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,18 +5,52 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     private var _score = 0
+    val score: Int
+        get() = _score
+
     private var _currentWordCount = 0
-    private var _currentScrambledWord = "test"
+    val currentWordCount: Int
+        get() = _currentWordCount
+
+    private lateinit var _currentScrambledWord: String
     val currentScrambledWord: String
         get() = _currentScrambledWord
-
-    init {
-        Log.d("GameFragment", "GameViewModel created!")
-    }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+
+    fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        tempWord.shuffle()
+
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)) {
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordsList.add(currentWord)
+        }
+    }
+
+    fun nextWord(): Boolean {
+        return if (currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
     }
 
 }
