eeb0cbae8a75ef010c3a23ab6bb52b6c5042bed8
Viktor Stashchuk <vstashchuk@reenbit.com>
2023-11-16 19:24:46 +0200

    Add observer for score and word count

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -61,13 +61,15 @@ class GameFragment : Fragment() {
         binding.skip.setOnClickListener { onSkipWord() }
         // Update the UI
         viewModel.currentScrambledWord.observe(viewLifecycleOwner) {newWord -> binding.textViewUnscrambledWord.text = newWord}
-        updateScoreOnScreen()
-    }
-
-    fun updateScoreOnScreen() {
-        binding.score.text = getString(R.string.score, viewModel.score)
-        binding.wordCount.text = getString(
-            R.string.word_count, viewModel.currentWordCount, MAX_NO_OF_WORDS)
+        viewModel.score.observe(viewLifecycleOwner
+        ) { newScore ->
+            binding.score.text = getString(R.string.score, newScore)
+        }
+        viewModel.currentWordCount.observe(viewLifecycleOwner
+        ) { newWordCount ->
+            binding.wordCount.text =
+                getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+        }
     }
 
     /*
@@ -85,7 +87,6 @@ class GameFragment : Fragment() {
         } else {
             setErrorTextField(true)
         }
-        updateScoreOnScreen()
     }
 
     /*
@@ -95,7 +96,6 @@ class GameFragment : Fragment() {
     private fun onSkipWord() {
         if (viewModel.nextWord()) {
             setErrorTextField(false)
-            updateScoreOnScreen()
         } else {
             showFinalScoreDialog()
         }
@@ -108,7 +108,6 @@ class GameFragment : Fragment() {
     private fun restartGame() {
         viewModel.reinitializeData()
         setErrorTextField(false)
-        updateScoreOnScreen()
     }
 
     /*
@@ -134,7 +133,7 @@ class GameFragment : Fragment() {
     private fun showFinalScoreDialog() {
         MaterialAlertDialogBuilder(requireContext())
             .setTitle(getString(R.string.congratulations))
-            .setMessage(getString(R.string.you_scored, viewModel.score))
+            .setMessage(getString(R.string.you_scored, viewModel.score.value))
             .setCancelable(false)
             .setNegativeButton(getString(R.string.exit)) { _, _ ->
                 exitGame()
